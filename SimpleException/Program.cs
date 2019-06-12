7946c3deb29fff48e793d6265105c1dba3234780
AlexandrTolstov <alextav_85@mail.ru>
2019-06-12 19:29:09 +0600

    1

--- SimpleException/Program.cs
+++ SimpleException/Program.cs
@@ -10,6 +10,15 @@ namespace SimpleException
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("****** Simple Exception Example *****");
+            Console.WriteLine("=> Creating a car and stepping on it");
+            Car myCar = new Car("Zippy", 20);
+            myCar.CrankTunes(true);
+            for (int i = 0; i < 10; i++)
+            {
+                myCar.Accelerate(10);
+            }
+            Console.ReadLine();
         }
     }
 }
